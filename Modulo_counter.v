d6c3fba51c38f676cda2df20f06d68f89fa76f8f
collingreen-del <collingreen@u.boisestate.edu>
2026-04-07 13:49:32 -0600

    Refactor increment logic and state reset for Modulo counter
    Updated the increment logic and reset conditions for state detection.

--- Modulo_counter.v
+++ Modulo_counter.v
@@ -8,18 +8,18 @@ module modulo_counter(
 wire q0, q1, q2;
 wire d0, d1, d2;
 
-/* increment logic (state + 1) */
+
 
 assign d0 = ~q0;
 assign d1 = q1 ^ q0;
 assign d2 = q2 ^ (q1 & q0);
 
-/* detect when state = 6 */
+
 
 wire is_five;
 assign is_five = q2 & ~q1 & q0;
 
-/* reset next state when reaching 6 */
+
 
 wire nd0, nd1, nd2;
 
@@ -27,7 +27,7 @@ assign nd0 = is_five ? 1'b0 : d0;
 assign nd1 = is_five ? 1'b0 : d1;
 assign nd2 = is_five ? 1'b0 : d2;
 
-/* instantiate DFFs */
+
 
 DFF ff0(clk, rst, nd0, q0);
 DFF ff1(clk, rst, nd1, q1);
@@ -35,7 +35,7 @@ DFF ff2(clk, rst, nd2, q2);
 
 assign state = {q2,q1,q0};
 
-/* output divider */
+
 
 wire out_q;
 wire out_d;
