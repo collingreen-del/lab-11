c74b04dee8405e0d5d67448a89968a4f7132d529
collingreen-del <collingreen@u.boisestate.edu>
2026-04-10 10:47:21 -0600

    Refactor Modulo_Counter to use is_six logic

--- Modulo_Counter.v
+++ Modulo_Counter.v
@@ -16,22 +16,22 @@ assign d2 = q2 ^ (q1 & q0);
 
 
 
-wire is_five;
-assign is_five = q2 & ~q1 & q0;
+wire is_six;
+assign is_six = q2 & ~q1 & q0;
 
 
 
 wire nd0, nd1, nd2;
 
-assign nd0 = is_five ? 1'b0 : d0;
-assign nd1 = is_five ? 1'b0 : d1;
-assign nd2 = is_five ? 1'b0 : d2;
+assign nd0 = is_six ? 1'b0 : ~q0;
+assign nd1 = is_six ? 1'b0 : (q1^q0);
+assign nd2 = is_six ? 1'b0 : (q2^(q1 & q0));
 
 
 
-DFF ff0(clk, rst, nd0, q0);
-DFF ff1(clk, rst, nd1, q1);
-DFF ff2(clk, rst, nd2, q2);
+d_ff ff0(clk, rst, nd0, q0);
+d_ff ff1(clk, rst, nd1, q1);
+d_ff ff2(clk, rst, nd2, q2);
 
 assign state = {q2,q1,q0};
 
@@ -40,9 +40,9 @@ assign state = {q2,q1,q0};
 wire out_q;
 wire out_d;
 
-assign out_d = is_five ^ out_q;
+assign out_d = is_six ? ~out_q : out_q;
 
-DFF ff_out(clk, rst, out_d, out_q);
+d_ff ff_out(clk,rst,out_d, out_q);
 
 assign out = out_q;
 
