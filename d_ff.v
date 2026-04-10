dddc1dbb12a251856a56185f752a7e7bb51b9d3b
collingreen-del <collingreen@u.boisestate.edu>
2026-04-10 10:48:07 -0600

    Refactor D flip flop for reset functionality

--- d_ff.v
+++ d_ff.v
@@ -1,26 +1,25 @@
 /*D flip flop module*/
 module d_ff (
-        /*
-        define inputs and outputs 
-        q is a "output reg" as its inside a 
-        "always" block.
-        */
+
+        
         input clk,
+        input rst,
         input d,
         output reg q,
         output q_wire
     );
 
-    // Initial state
-    initial begin
-        q = 1'b0;
-    end
+ 
+    
 
-    always @(posedge clk) begin
+    always @(posedge clk or posedge rst) begin
+    if(rst) 
+    q <= 1'b0;
+    else
         q <= d;
     end
 
-    // output; this uses a wire that is connected in top
+    
     assign q_wire = ~q;
 
 endmodule
